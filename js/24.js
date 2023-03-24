dfda9dfa2603f81c5dafe2985703cb2e15c6feee
Edgard <edgard_ortega@live.cl>
2023-03-24 17:49:42 -0300

    repaso JS

--- js/24.js
+++ js/24.js
@@ -1,12 +1,21 @@
 // Eventos del DOM -clicks
 
-// const heading = document.querySelector('.heading')
-// heading.addEventListener('click', () => {
-//     console.log('Diste click en heading')
-// })
-
-const clickHeading = () => {
-    console.log('Diste click en heading')
-}
 const heading = document.querySelector('.heading')
-heading.addEventListener('click', clickHeading) // clickheading debe declararse primero antes de usarse no puede ser al revez
+heading.addEventListener('click', () => {
+    console.log('Diste click en heading')
+})
+
+// const clickHeading = () => {
+//     console.log('Diste click en heading')
+// }
+// const heading = document.querySelector('.heading')
+// heading.addEventListener('click', clickHeading) // clickheading debe declararse primero antes de usarse no puede ser al revez
+
+
+const enlaces = document.querySelectorAll('.navegacion a')
+
+enlaces.forEach(enlace => { // no puede ser
+    enlace.addEventListener('click', () => {
+        console.log('Diste click en un enlace')
+    })
+})
---
+++ js/25.js
@@ -0,0 +1,18 @@
+// Eventos del DOM - Imputs
+
+const inputNombre = document.querySelector('.nombre')
+
+inputNombre.addEventListener('input', function(e) {
+    console.log(e.target.value)
+})
+
+const inputPassword = document.querySelector('.password')
+inputPassword.addEventListener('input', funcionPassword)
+
+function funcionPassword() {
+    inputPassword.type = 'text'
+
+    setTimeout(() => {
+        inputPassword.type = 'password'
+    }, 100);
+}
