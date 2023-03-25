b78c0ee89899e51ac45585dcbee5c7ea2e989d4d
Edgard <edgard_ortega@live.cl>
2023-03-25 01:45:57 -0300

    import y export

---
+++ js/28.js
@@ -0,0 +1,10 @@
+// Importar y Exports
+
+import { sumar } from "./funciones.js"
+import { restar } from "./funciones.js"
+
+const resultado = sumar(2, 5)
+const resultado2 = restar(2, 5)
+
+console.log(resultado)
+console.log(resultado2)
